f539963ccf109b1b918deecdc33d724376f600cd
any-yano-kentaro <yano@anysense.co.jp>
2023-04-10 09:30:04 +0900

    get chat messages

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,6 +4,18 @@ class ChatsController < ApplicationController
         video_id = "jfKfPfyJRdk"
         
         client = GoogleService.new
-        @chat_id = client.get_live_details(video_id)
+
+        live_details = client.get_live_details(video_id)
+        live_chat_id = live_details.items[0].live_streaming_details.active_live_chat_id
+
+        @live_chat_messages = set_chat_messages(live_chat_id)
+    end
+
+    def set_chat_messages(live_chat_id)
+        client = GoogleService.new
+
+        live_chat_message_info = client.get_live_chat_message_info(live_chat_id)
+        
+        return live_chat_message_info.items
     end
 end
--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -6,12 +6,23 @@ class GoogleService
 
   def get_live_details(video_id, after: 1.months.ago, before: Time.now)
     service = Google::Apis::YoutubeV3::YouTubeService.new
-    service.key = ""
+    service.key = "AIzaSyC2Y2Ktg67wO1Z4ZfO_ZxIhesV06fn95Hs"
   
     next_page_token = nil
     begin
       live_streaming_details = service.list_videos("liveStreamingDetails", id: video_id)
-      return live_streaming_details.to_json
+      return live_streaming_details
     end
   end
+
+  def get_live_chat_message_info(live_chat_id)
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = "AIzaSyC2Y2Ktg67wO1Z4ZfO_ZxIhesV06fn95Hs"
+
+    next_page_token = nil
+
+    chat_message_info = service.list_live_chat_messages(live_chat_id, "snippet")
+    return chat_message_info
+  end
+
 end
