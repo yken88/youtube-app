458248301f46f811294ff682fb993fcb23e3b22b
any-yano-kentaro <yano@anysense.co.jp>
2023-05-03 19:49:14 +0900

    when live streaming is inactive rendor chats top

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -7,19 +7,25 @@ class ChatsController < ApplicationController
     private
 
     def search
+        @live_chat_messages = []
         if params[:video_id]
-            video_id = params[:video_id]
+            @live_chat_messages = live_chat_messages(params[:video_id])
         end
-
-        @live_chat_messages = live_chat_messages(video_id)
     end
 
     def live_chat_messages(video_id)
         client = GoogleService.new
 
-        live_chat_id = client.get_live_details(video_id).items[0].live_streaming_details.active_live_chat_id
-        live_chat_message_info = client.get_live_chat_message_info(live_chat_id)
+        live_detail = client.get_live_details(video_id).items[0]
+        live_chat_id = live_detail.live_streaming_details.active_live_chat_id
+
+        if live_chat_id
+            live_chat_message_info = client.get_live_chat_message_info(live_chat_id).items
+        else
+            flash[:nolive] = 'this video is not live'
+            redirect_to "/chats"
+        end
         
-        return live_chat_message_info.items
+        return live_chat_message_info
     end
 end
