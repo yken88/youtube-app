dc71b7c643572a9f999e55211d8519846b50f09b
any-yano-kentaro <yano@anysense.co.jp>
2023-05-03 19:27:05 +0900

    add search function

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,19 +1,23 @@
 class ChatsController < ApplicationController
+    before_action :search, only: [:index]
+
     def index
-        # TODO:リクエストから取れるようにする
-        video_id = "jfKfPfyJRdk"
-        
-        client = GoogleService.new
-
-        live_details = client.get_live_details(video_id)
-        live_chat_id = live_details.items[0].live_streaming_details.active_live_chat_id
-
-        @live_chat_messages = set_chat_messages(live_chat_id)
     end
 
-    def set_chat_messages(live_chat_id)
+    private
+
+    def search
+        if params[:video_id]
+            video_id = params[:video_id]
+        end
+
+        @live_chat_messages = live_chat_messages(video_id)
+    end
+
+    def live_chat_messages(video_id)
         client = GoogleService.new
 
+        live_chat_id = client.get_live_details(video_id).items[0].live_streaming_details.active_live_chat_id
         live_chat_message_info = client.get_live_chat_message_info(live_chat_id)
         
         return live_chat_message_info.items
